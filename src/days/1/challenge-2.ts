90f1a99c673815ff016d6bfb447e772d37af3e15
Ugurcan Sengit <ugurcansengit@gmail.com>
2019-01-14 15:54:19 +0100

    Fix a naming issue

--- src/days/1/challenge-2.ts
+++ src/days/1/challenge-2.ts
@@ -1,32 +1,32 @@
 type FrequencyState = {
   currentFrequency: number,
-  isCalibrationCompled: boolean,
+  isCalibrationCompleted: boolean,
   previousFrequencies?: Set<number>
 }
 
 const initialFrequencyState: FrequencyState = {
   currentFrequency: 0,
-  isCalibrationCompled: false,
+  isCalibrationCompleted: false,
   previousFrequencies: new Set<number>().add(0)
 };
 
 const toFrequency = (number: string): FrequencyState => ({ currentFrequency: Number(number) } as FrequencyState);
 
 const calibrate = (prevFrequency: FrequencyState, currentFrequency: FrequencyState): FrequencyState => {
-  if (prevFrequency.isCalibrationCompled) return prevFrequency;
+  if (prevFrequency.isCalibrationCompleted) return prevFrequency;
 
   const totalFrequency: number = prevFrequency.currentFrequency + currentFrequency.currentFrequency;
 
   if(prevFrequency.previousFrequencies.has(totalFrequency)) {
     return {
       currentFrequency: totalFrequency,
-      isCalibrationCompled: true
+      isCalibrationCompleted: true
     };
   } else {
     return {
       currentFrequency: totalFrequency,
       previousFrequencies: prevFrequency.previousFrequencies.add(totalFrequency),
-      isCalibrationCompled: false
+      isCalibrationCompleted: false
     }
   }
 };
